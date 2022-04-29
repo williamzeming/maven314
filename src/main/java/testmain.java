e6892621dedc44b9a6e4ab30fab3459969cfa462
zeming666 <zr109@uowmail.edu.au>
2022-04-29 14:48:49 +1000

    2

---
+++ src/main/java/testmain.java
@@ -0,0 +1,12 @@
+public class testmain {
+    public static void main(String[] args) {
+        System.out.println(cul());
+    }
+    public static int cul(){
+        int x = 3;
+        int y = 4;
+        int res;
+        res = x+y;
+        return res;
+    }
+}
